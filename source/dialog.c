aa3f2c233eaefe0265c1ac1e843fd3cc81a766ea
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-06-17 00:30:28 -0500

    Update `init_loading_screen()` to use waiting load screen

--- source/dialog.c
+++ source/dialog.c
@@ -107,8 +107,20 @@ void update_progress_bar(uint64_t progress, const uint64_t total_size, const cha
 
 int init_loading_screen(const char* message)
 {
-	init_progress_bar(message);
-    return 1;
+	OrbisMsgDialogButtonsParam buttonsParam;
+	OrbisMsgDialogUserMessageParam messageParam;
+	OrbisMsgDialogParam dialogParam;
+
+	sceMsgDialogInitialize();
+	orbisMsgDialogParamInitialize(&dialogParam);
+	memset(&buttonsParam, 0, sizeof(buttonsParam));
+	memset(&messageParam, 0, sizeof(messageParam));
+	dialogParam.userMsgParam = &messageParam;
+	dialogParam.mode = ORBIS_MSG_DIALOG_MODE_USER_MSG;
+	messageParam.buttonType = ORBIS_MSG_DIALOG_BUTTON_TYPE_WAIT;
+	messageParam.msg = message;
+
+	return (sceMsgDialogOpen(&dialogParam) >= 0);
 }
 
 void stop_loading_screen()

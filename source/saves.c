449eb07fde74de690bdd5ace5e15e775a046ec25
tonytwostep <43851175+tonytwostep@users.noreply.github.com>
2026-03-08 16:11:45 -0400

    Bulk FTP Save Management (#252)
    * bulk ftp commands as enum members
    
    * conditionally display ftp bulk menu options if server is configured
    
    * first stab at `uploadAllSavesFTP` function
    
    * helper function `_upload_save_ftp` for shared code between single/bulk ftp methods
    
    Ftp save uploads are only allowed from HDD
    
    * Add temporary variable for path management in FTP upload
    
    ---------
    
    Co-authored-by: Damián Parrino <bucanero@users.noreply.github.com>

--- source/saves.c
+++ source/saves.c
@@ -1800,6 +1800,15 @@ list_t * ReadUserList(const char* userPath)
 	_createOptions(cmd, _("Copy Saves to USB"), CMD_COPY_ALL_SAVES_USB);
 	list_append(item->codes, cmd);
 
+	if (apollo_config.ftp_url[0])
+	{
+		cmd = _createCmdCode(PATCH_COMMAND, CHAR_ICON_NET " ", _("Upload selected Saves to FTP"), CMD_UPLOAD_SAVES);
+		list_append(item->codes, cmd);
+
+		cmd = _createCmdCode(PATCH_COMMAND, CHAR_ICON_NET " ", _("Upload all Saves to FTP"), CMD_UPLOAD_ALL_SAVES);
+		list_append(item->codes, cmd);
+	}
+
 	cmd = _createCmdCode(PATCH_COMMAND, CHAR_ICON_NET " ", _("Start local Web Server"), CMD_SAVE_WEBSERVER);
 	list_append(item->codes, cmd);
 

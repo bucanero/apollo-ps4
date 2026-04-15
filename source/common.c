67466b293f1d1737d857d568256be54acda05dfe
Damian Parrino <bucanero@users.noreply.github.com>
2026-04-15 12:08:13 -0300

    add zh_tw support

--- source/common.c
+++ source/common.c
@@ -289,8 +289,10 @@ const char * get_user_language(void)
         return "ko";
 
     case ORBIS_SYSTEM_PARAM_LANG_CHINESE_T:            // 10   Chinese (traditional)
+        return "zh_tw";
+
     case ORBIS_SYSTEM_PARAM_LANG_CHINESE_S:            // 11   Chinese (simplified)
-        return "zh";
+        return "zh_cn";
 
     case ORBIS_SYSTEM_PARAM_LANG_FINNISH:              // 12   Finnish
         return "fi";
